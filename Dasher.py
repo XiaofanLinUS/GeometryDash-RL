4bf3fd0179f99cfc2168c3ca8247032cf69f97a2
xiaofanlinus <xiaofanlinus@gmail.com>
2018-12-17 17:42:44 -0800

    working on memory problem

--- Dasher.py
+++ Dasher.py
@@ -9,21 +9,24 @@ class Dasher(nn.Module):
         # expect input size: 400x400x1
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=5, kernel_size=3, stride=1, padding=0)
         self.conv2 = nn.Conv2d(in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=0)
-        #self.conv3 = nn.Conv2d(in_channels=10, out_channels=12, kernel_size=7, stride=1)
+        self.conv3 = nn.Conv2d(in_channels=5, out_channels=5, kernel_size=7, stride=1)
         self.pool_half = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(in_features=194045, out_features=128)
+        self.fc1 = nn.Linear(in_features=188180, out_features=128)
         self.fc2 = nn.Linear(in_features=128, out_features=1)
+        self.reward_decay = 0.8
+
         # state(frame of game)
-        self.states = []
+        # self.states = []
+
         # gradient(history of decisions made upon sampling)
-        self.probs = []
+        self.log_probs = []
         # rewards(history of rewards check whether current move is good or bad)
         self.rewards = []
 
     def forward(self, x):
         x = F.elu(self.conv1(x))
         x = F.elu(self.conv2(x))
-        #x = F.elu(self.conv3(x))
+        x = F.elu(self.conv3(x))
         x = self.pool_half(x)
         x = x.view(x.numel())
         x = F.elu(self.fc1(x))
@@ -31,11 +34,41 @@ class Dasher(nn.Module):
         
         return x
 
-    def makeMove(self, x):
-        #self.states.append(x)
+    def conclude_loss(self):
+        # lost decay calculation
+        reward_so_far = 0
+        # corrected reward
+        corrected_reward = torch.empty(len(self.rewards))
+        for idx, reward in enumerate(reversed(self.rewards)):
+            reward_so_far = self.reward_decay * reward_so_far + reward
+            corrected_reward[idx] = reward_so_far
+
+        # normalized
+        corrected_reward = (corrected_reward - corrected_reward.mean()) / corrected_reward.std()
+
+        corrected_reward = corrected_reward.cuda()
+        log_probs = torch.tensor(self.log_probs).cuda()
+
+        final_loss = -log_probs * corrected_reward
+
+        # reset for next game
+        self.reset()
+
+        return final_loss
+
+    def reset(self):
+        # self.states = []
+        self.log_probs = []
+        self.rewards = []
+
+    def save_reward(self, r):
+        self.rewards.append(r)
+
+    def make_move(self, x):
+        # self.states.append(x)
         probability = self.forward(x)
         probability = Bernoulli(probability)
         move = probability.sample()
-        #self.probs.append(-probability.log_prob(move))
+        # self.log_probs.append(probability.log_prob(move))
         return move.item()
 
--- screen_capture.py
+++ screen_capture.py
@@ -1,28 +1,49 @@
 from Game import Game
 from Dasher import Dasher
-import time 
+
+
+import time
 import torch
 
-def screen_record(): 
+
+def train_an_eipsode(model, opt):
+        opt.zero_grad()        
+        loss = model.conclude_loss()
+        loss.backward()
+        opt.step()
+
+def play_the_game(): 
         game = Game()
+
         model = Dasher()
         model = model.cuda()
+        optmizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+        # init move
         move = 0
         while(game.running):
                 reward, pause, input_img = game.get_report(move)
+
+                # feed image input to the network
                 input_img_tensor = torch.tensor(input_img).float()
                 input_img_tensor = input_img_tensor.unsqueeze(0)
                 input_img_tensor = input_img_tensor.unsqueeze(0).cuda()
-                move = model.makeMove(input_img_tensor)
+
+                # make move upon the image
+                move = model.make_move(input_img_tensor)
+
+                # save the reward for last move
+                model.save_reward(reward)
                 print(move)
                 if pause:
-                        time.sleep(10)
+                        train_an_eipsode(model, optmizer)
+                        time.sleep(1)
                         game.reset()
 
 
                 
 
-screen_record()
+play_the_game()
 
 
 
